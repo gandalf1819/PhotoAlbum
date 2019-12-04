050a4d45ab935d18477f2b43dc26ccd6cf6f4999
Chinmay Wyawahare <chinmaynw@gmail.com>
2019-12-03 22:28:57 -0500

    Fixed front-end transcription for voice search

--- views/js/main.js
+++ views/js/main.js
@@ -1,6 +1,158 @@
 // API Gateway SDK
 var apigClient = apigClientFactory.newClient();
 
+URL = window.URL || window.webkitURL;
+
+var gumStream; 						//stream from getUserMedia()
+var rec; 							//Recorder.js object
+var input; 							//MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext //audio context to help us record
+
+var recordButton = document.getElementById("start-btn");
+var stopButton = document.getElementById("pause-btn");
+
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+
+// Start Transcription
+function startRecording() {
+	console.log("recordButton clicked");
+
+	/*
+		Simple constraints object, for more advanced audio features see
+		https://addpipe.com/blog/audio-constraints-getusermedia/
+	*/
+    
+    var constraints = { audio: true, video:false }
+
+ 	/*
+    	Disable the record button until we get a success or fail from getUserMedia() 
+	*/
+
+	recordButton.disabled = true;
+	stopButton.disabled = false;
+
+	/*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
+
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+
+		/*
+			create an audio context after getUserMedia is called
+			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+			the sampleRate defaults to the one set in your OS for your playback device
+
+		*/
+		audioContext = new AudioContext();
+
+		//update the format 
+		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+
+		/*  assign to gumStream for later use  */
+		gumStream = stream;
+		
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
+
+		/* 
+			Create the Recorder object and configure to record mono sound (1 channel)
+			Recording 2 channels  will double the file size
+		*/
+		rec = new Recorder(input,{numChannels:1})
+
+		//start the recording process
+		rec.record()
+
+		console.log("Recording started");
+
+	}).catch(function(err) {
+	  	//enable the record button if getUserMedia() fails
+    	recordButton.disabled = false;
+    	stopButton.disabled = true;
+	});
+}
+
+// Stop Transcription
+function stopRecording() {
+	console.log("stopButton clicked");
+
+	//disable the stop button, enable the record too allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = false;
+	
+	//tell the recorder to stop the recording
+	rec.stop();
+
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
+
+	//create the wav blob and pass it on to createDownloadLink
+	rec.exportWAV(createDownloadLink);
+}
+
+// Get the audio file for transcription
+function createDownloadLink(blob) {
+	
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+    var link = document.createElement('a');
+    
+    searchPhotosS3(blob);
+
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = new Date().toISOString();
+
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
+
+	//save to disk link
+	link.href = url;
+	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+	link.innerHTML = "Save to disk";
+
+	//add the new audio element to li
+	li.appendChild(au);
+	
+	//add the filename to the li
+	li.appendChild(document.createTextNode(filename+".wav "))
+
+	//add the save to disk link to li
+	li.appendChild(link);
+	
+    recordingsList.appendChild(li);
+}
+
+// Search image results
+$('#search-btn').click(function() {
+    query = $('#search-phrase').val();
+    params = {q: query};
+    apigClient.searchGet(params, {}, {})
+        .then(function(result){
+        
+        console.log(result);
+        
+        let img_list = result.data
+        for (var i = 0; i < img_list.length; i++) {
+            img_url = img_list[i];
+            new_img = document.createElement('img');
+            new_img.src = img_url;
+            document.body.appendChild(new_img);
+        }
+        
+        }).catch(function(result){
+            console.log(result);
+        });
+});
+
 // Expand the search icon
 $(function () {
     $(".search-input").on("click", function() {
@@ -45,17 +197,12 @@ $(document).ready(function(){
     });
 });
 
-var rec=Recorder({
-    bitRate:32,
-    sampleRate:24000
-});
-
 // Image search using transcription
 function searchPhotosS3(data) {
     let config = {
             headers:{'Content-Type': data.type}
     };
-    url = 'https://3ff1u19bck.execute-api.us-east-1.amazonaws.com/Prod/upload/transcribe-notes/test.mp3'
+    url = 'https://3ff1u19bck.execute-api.us-east-1.amazonaws.com/Prod/upload/transcribe-notes/test.wav'
     axios.put(url,data,config).then(response=>{
         console.log(response.data)
         // alert("Upload successful!!");
@@ -98,57 +245,3 @@ function searchPhotosS3(data) {
         console.log(err);
     })
 }
-
-// Start transcription
-$(".start-btn").on("click", function() {
-    alert('start-btn clicked');
-    // record.disabled = true;
-    // start-btn.style.backgroundColor = "blue"
-    // stopRecord.disabled=false;
-    rec.open(function(){
-        rec.start();
-    },function(msg,isUserNotAllow){
-        console.log((isUserNotAllow?"UserNotAllow，":"")+"can't record:"+msg);
-    });
-});
-
-// Stop transcription
-$(".pause-btn").on("click", function() {
-    alert("pause-btn clicked")
-    // record.disabled = false;
-    // stop.disabled=true;
-    // record.style.backgroundColor = "red"
-    rec.stop(function(blob,duration){
-        console.log(URL.createObjectURL(blob),"Duration:"+duration+"ms");
-        console.log(blob);
-        rec.close();
-        var audio=document.createElement("audio");
-        audio.controls=true;
-        document.body.appendChild(audio);
-        audio.src=URL.createObjectURL(blob);
-        // audio.play();
-        searchPhotosS3(blob);
-    });
-});
-
-// Search image results
-$('#search-btn').click(function() {
-    query = $('#search-phrase').val();
-    params = {q: query};
-    apigClient.searchGet(params, {}, {})
-        .then(function(result){
-        
-        console.log(result);
-        
-        let img_list = result.data
-        for (var i = 0; i < img_list.length; i++) {
-            img_url = img_list[i];
-            new_img = document.createElement('img');
-            new_img.src = img_url;
-            document.body.appendChild(new_img);
-        }
-        
-        }).catch(function(result){
-            console.log(result);
-        });
-});
