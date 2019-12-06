3648e5e3bdb910fcee084abcc2497ea64a43537c
Chinmay Wyawahare <chinmaynw@gmail.com>
2019-12-06 03:35:01 -0500

    Fixed transcribe bug for LF2

--- lambda-functions/image_search.py
+++ lambda-functions/image_search.py
@@ -1,11 +1,12 @@
 import json
-
+import time
 import boto3
 import requests
 
 ES_HOST = 'https://vpc-photosearch-n3iu72etjtdnclayan6fq44twe.us-east-1.es.amazonaws.com'
 REGION = 'us-east-1'
 
+job_name = ""
 
 def get_url(es_index, es_type):
     url = ES_HOST + '/' + es_index + '/' + es_type + '/_search'
@@ -14,8 +15,8 @@ def get_url(es_index, es_type):
 
 def get_slots_from_lex(query):
     lex = boto3.client('lex-runtime')
-
-    # AWS Lex
+    
+    # AWS Lex 
     print("query:{}".format(query))
     lex_response = lex.post_text(
         botName='AlbumBot',
@@ -76,50 +77,54 @@ def build_response(code, body):
         'body': json.dumps(body)
     }
 
-
 def lambda_handler(event, context):
     # recieve from API Gateway
     print("EVENT --- {}".format(json.dumps(event)))
+
+    global job_name
+    # return {
+    #     'statusCode': 200,
+    #     'body': json.dumps('Hello from Lambda!')
+    # }
     query_params = event["queryStringParameters"]
     if not query_params:
         return build_response(400, "Bad request, there was nothing in the query params")
     query = query_params["q"]
-    print("QueryStringParameters: ----", query)
-
+    print("QueryStringParameters: ----" , query)
+    
     # AWS Transcribe: Get transcribed text from voice recordings
     if (query == "transcriptionStart"):
         transcribe = boto3.client('transcribe')
-        job_name = time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()).replace(":",
-                                                                                   "-").replace(
-            " ", "")
-        job_uri = "https://s3.amazonaws.com/transcribe-notes/test.wav"
+        job_name = time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()).replace(":", "-").replace(" ", "")
+        storage_uri = "transcriptionnotes"
+        job_uri = "https://s3.amazonaws.com/transcriptionnotes/test.wav"
         transcribe.start_transcription_job(
             TranscriptionJobName=job_name,
             Media={'MediaFileUri': job_uri},
             MediaFormat='wav',
-            LanguageCode='en-US'
+            LanguageCode='en-US',
+            OutputBucketName=storage_uri
         )
         while True:
             status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
-            if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED',
-                                                                        'FAILED']:
+            if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
                 break
             print("Transcription not ready yet!")
             time.sleep(5)
         print("Transcript URL: ", status)
         transcriptURL = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
         trans_text = requests.get(transcriptURL).json()
-
+        
         print("Transcripts: ", trans_text)
         print(trans_text["results"]['transcripts'][0]['transcript'])
-
+        
         s3client = boto3.client('s3')
         response = s3client.delete_object(
-            Bucket='transcribe-notes',
+            Bucket='transcriptionnotes',
             Key='test.wav'
         )
         query = trans_text["results"]['transcripts'][0]['transcript']
-        s3client.put_object(Body=query, Bucket='transcribe-notes', Key='test.wav')
+        s3client.put_object(Body=query, Bucket='transcriptionnotes', Key='test.wav')
         return {
             'statusCode': 200,
             'headers': {
@@ -129,17 +134,27 @@ def lambda_handler(event, context):
             },
             'body': "Transcription completed"
         }
-
+    
     if (query == "transcriptionEnd"):
         s3client = boto3.client('s3')
-        data = s3client.get_object(Bucket='transcribe-notes', Key='test.wav')
-        query = data.get('Body').read().decode('utf-8')
+        jb_name = job_name + '.json'
+        print("Job name: ", jb_name)
+        
+        # Get Transcription (JSON) from s3 bucket
+        s3 = boto3.resource('s3')
+        obj = s3.Object('transcriptionnotes', jb_name)
+        body = obj.get()['Body'].read()  
+        
+        dat = body.decode('utf8').replace("'", '"')
+        data = json.loads(dat)
+        query = data["results"]["transcripts"][0]["transcript"]
+        
         print("Voice query: ", query)
         s3client.delete_object(
-            Bucket='transcribe-notes',
+            Bucket='transcriptionnotes',
             Key='test.wav'
         )
-
+    
     slots, valid = get_slots_from_lex(query)
     if not valid:
         build_response(200, "I could not understand what you want")
@@ -149,4 +164,4 @@ def lambda_handler(event, context):
         return build_response(200, img_list)
     else:
         return build_response(200,
-                              "There were no photos matching the categories you were looking for.")
+                       "There were no photos matching the categories you were looking for.")
